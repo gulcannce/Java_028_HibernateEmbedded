3f689244539d9daae949536819fd76da688b6d46
gulcan <gulcanncee@gmail.com>
2024-11-26 22:52:16 +0300

    Initial commit

---
+++ src/main/java/com/gulcancelik/model/Worker.java
@@ -0,0 +1,93 @@
+package com.gulcancelik.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "WORKERS")
+public class Worker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "WORKER_ID")
+    private int id;
+
+    @Column(name = "WORKER_NUMBER")
+    private int workerNumber;
+
+    @Column(name = "FIRST_NAME", length = 60, nullable = false)
+    private String firstName;
+
+    @Column(name = "LAST_NAME", length = 75)
+    private String lastName;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "CREATE_DATE")
+    private Date createDate;
+
+    @Embedded
+    private Address address;
+
+
+    public Worker() {
+    }
+
+    public Worker(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Worker(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public int getWorkerNumber() {
+        return workerNumber;
+    }
+
+    public void setWorkerNumber(int workerNumber) {
+        this.workerNumber = workerNumber;
+    }
+}
